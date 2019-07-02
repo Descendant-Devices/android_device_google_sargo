5091fba0d513038cec778320c640939d9ae2574e
ZVNexus <zvnexus@outlook.com>
2019-07-02 01:59:40 -0400

    Update fingerprint to PQ3B.190705.003

--- statix_sargo.mk
+++ statix_sargo.mk
@@ -30,8 +30,8 @@ PRODUCT_MODEL := Pixel 3a
 # Device fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sargo \
-    BUILD_FINGERPRINT=google/sargo/sargo:9/PQ3B.190605.006/5560048:user/release-keys \
-    PRIVATE_BUILD_DESC="sargo-user 9 PQ3B.190605.006 5560048 release-keys"
+    BUILD_FINGERPRINT=google/sargo/sargo:9/PQ3B.190705.003/5622519:user/release-keys \
+    PRIVATE_BUILD_DESC="sargo-user 9 PQ3B.190705.003 5622519 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/sargo/sargo-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
